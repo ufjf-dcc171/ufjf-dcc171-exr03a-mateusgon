6956dd64b6651634118a0d3a80f7aeb982199d5c
ice <ice@lab06m27.nrc.ice.ufjf.br>
2017-08-17 22:37:03 -0300

    Parte 1 - Iniciando as lacunas

---
+++ src/br/ufjf/dcc171/Aula04Exr3a.java
@@ -0,0 +1,15 @@
+package br.ufjf.dcc171;
+
+import javax.swing.JFrame;
+
+public class Aula04Exr3a {
+
+    public static void main(String[] args) {
+        Janela calcula = new Janela();
+        calcula.setSize(200, 400);
+        calcula.setLocationRelativeTo(null);
+        calcula.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        calcula.setVisible(true);
+    }
+    
+}
---
+++ src/br/ufjf/dcc171/Janela.java
@@ -0,0 +1,38 @@
+package br.ufjf.dcc171;
+
+import java.awt.FlowLayout;
+import java.awt.HeadlessException;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+class Janela extends JFrame {
+
+    private final JLabel etiqueta;
+    private final JLabel etiqueta2;
+    private final JLabel etiqueta3;
+   // private final JLabel etiqueta4;
+    private final JTextField texto;
+    private final JTextField texto2;
+    private final JTextField texto3;
+    
+    public Janela() throws HeadlessException {
+            super ("Equação do Segundo Grau");
+            setLayout (new FlowLayout());
+            etiqueta = new JLabel("Coeficiente a:");
+            texto = new JTextField(10);
+            etiqueta2 = new JLabel ("Coeficiente b:");
+            texto2 = new JTextField(10);
+            etiqueta3 = new JLabel ("Coeficiente c:");
+            texto3 = new JTextField(10);
+            
+            add(etiqueta);
+            add(texto);
+            add(etiqueta2);
+            add(texto2);
+            add(etiqueta3);
+            add(texto3);
+    }
+    
+    
+}
